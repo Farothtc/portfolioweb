bdb0c761c00e6a98d71f60bb0eea08b6b91f48b0
Farothtc <mvkirimi@gmail.com>
2025-03-26 20:01:52 +0300

    feat: general page structure improved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,20 @@
 import Particles from "../components/Particles";
+import Nav from "../components/Nav";
 
 export default function About() {
+  const style = {
+    backgroundImage: "linear-gradient(to right, #171719, #111112)",
+  };
   return (
-    <>
+    <div className="h-screen" style={style}>
+      <Nav />
       <div>
-        <Particles
+        {/* <Particles
           className="absolute w-screen h-screen inset-0"
           quantity={100}
-        />
+        /> */}
       </div>
       <h1>Hello I am about</h1>
-    </>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+import { useEffect } from "react";
 import Particles from "./components/Particles";
 import Link from "next/link";
+import anime from "animejs";
 
 export default function Home() {
+  useEffect(() => {
+    anime({
+      targets: ".mustafa",
+      opacity: [0, 1],
+      translateY: [20, 0],
+      easing: "easeOutQuad",
+      duration: 750,
+      complete: () => {
+        anime({
+          targets: ".title",
+          opacity: [0, 1],
+          translateY: [20, 0],
+          easing: "easeOutQuad",
+          duration: 750,
+          complete: () => {
+            anime({
+              targets: [".aboutMe", ".projectsMe", ".certiMe", ".contactMe"],
+              opacity: [0, 1],
+              translateY: [20, 0],
+              easing: "easeOutQuad",
+              duration: 750,
+              delay: anime.stagger(400),
+            });
+          },
+        });
+      },
+    });
+  }, []);
   return (
     <>
       <div>
@@ -11,26 +42,34 @@ export default function Home() {
         />
       </div>
       <div className="relative h-screen">
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-4 rounded-lg shadow-md flex flex-col justify-center items-center gap-5">
-          <h1 className="text-4xl">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-4 rounded-lg shadow-md flex flex-col justify-center items-center gap-5 w-auto md:flex-col">
+          <h1 className="text-2xl sm:text-4xl mustafa">
             Hello, I'm <strong>Mustafa</strong>.
           </h1>
-          <h2 className="text-4xl">I'm a Jr. Front-End Web Developer.</h2>
-          <div className="flex flex-row pt-3 gap-3">
+          <h2 className="text-lg sm:text-4xl whitespace-nowrap title">
+            I'm a Jr. Front-End Web Developer.
+          </h2>
+          <div className="flex flex-col pt-3 gap-3 sm:flex-row">
             <Link href="/about">
-              <button className="btn btn-circle btn-outline btn-lg w-40">
+              <button className="btn btn-circle btn-outline btn-sm sm:btn-lg w-40 aboutMe">
                 About Me
               </button>
             </Link>
-            <button className="btn btn-circle btn-outline btn-lg w-40">
-              Projects
-            </button>
-            <button className="btn btn-circle btn-outline btn-lg w-40">
-              Certificates
-            </button>
-            <button className="btn btn-circle btn-outline btn-lg w-40">
-              Contact
-            </button>
+            <Link href="/projects">
+              <button className="btn btn-circle btn-outline btn-sm sm:btn-lg w-40 projectsMe">
+                Projects
+              </button>
+            </Link>
+            <Link href="/certificates">
+              <button className="btn btn-circle btn-outline btn-sm sm:btn-lg w-40 certiMe">
+                Certificates
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="btn btn-circle btn-outline btn-sm sm:btn-lg w-40 contactMe">
+                Contact
+              </button>
+            </Link>
           </div>
         </div>
       </div>
