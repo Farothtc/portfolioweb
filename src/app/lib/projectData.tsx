79c3b133060cc135a71097f5e0db42291fedc314
Farothtc <mvkirimi@gmail.com>
2025-05-22 16:35:54 +0300

    fix: new content added and readme fixed

--- src/app/lib/projectData.tsx
+++ src/app/lib/projectData.tsx
@@ -8,14 +8,30 @@ type ProjectData = {
 const projectData: ProjectData[] = [
   {
     id: 1,
+    name: "Browser Extension Manager",
+    description:
+      "Programmed carefully a dynamic and fully responsive SPA that aims to reflect a Browser Extension Manager. Built with Next.js, TypeScript, daisyUI, Tailwind CSS. Deployed with Vercel for better performance.",
+    date: "Apr - May 2025",
+    link: "https://browser-extension-manager-five.vercel.app",
+  },
+  {
+    id: 2,
+    name: "Interactive Card",
+    description:
+      "Constructed a dynamic and fully responsive SPA that aims to reflect a payment page FrontEnd. Built with Next.js, TypeScript, shadcn, Tailwind CSS. Deployed with Vercel for better performance.",
+    date: "Apr - May 2025",
+    link: "https://interactive-card-five.vercel.app",
+  },
+  {
+    id: 3,
     name: "mvkirimi.com",
     description:
-      "A dynamic and fully responsive SPA that aims to act as a personal portfolio site. Built with Next.js, TypeScript, daisyUI, Tailwind CSS and anime.js. Deployed with Vercel for better performance.",
+      "A dynamic and fully responsive SPA that aims to act as a personal portfolio site. Built with Next.js, TypeScript, daisyUI, Tailwind CSS, anime.js and Framer Motion. Deployed with Vercel for better performance.",
     date: "Jan - Apr 2025",
     link: "https://mvkirimi.com",
   },
   {
-    id: 2,
+    id: 4,
     name: "minedify.com",
     description:
       " A dynamic SPA on mineable cryptocurrencies that is fully responsive including Next.js, JavaScript, daisyUI, Tailwind CSS, anime.js. Deployed with Vercel for better optimization and performance.",
@@ -23,7 +39,7 @@ const projectData: ProjectData[] = [
     link: "https://minedify.com",
   },
   {
-    id: 3,
+    id: 5,
     name: "Pokedex",
     description:
       "A react site built for pokemon information. Whole site is built with React.ts, React Router and Bootstrap. API calls made with Axios. As module bundler Vite is preferred. VCS was Git. Deployed with Vercel.",
@@ -31,7 +47,7 @@ const projectData: ProjectData[] = [
     link: "https://pokedex-eta-murex.vercel.app",
   },
   {
-    id: 4,
+    id: 6,
     name: "ToDo-Next",
     description:
       "A Next.js project built for task tracking. Whole site is built with Next.js, JavaScript, anime.js, daisyUI and Tailwind CSS. VCS was Git. Deployed with Vercel.",
@@ -39,7 +55,7 @@ const projectData: ProjectData[] = [
     link: "https://todo-next-two-beta.vercel.app",
   },
   {
-    id: 5,
+    id: 7,
     name: "piriltivan.com",
     description:
       "Static site built for information and customer trust. Built by React.js and Bootstrap. As module bundler Vite is preferred. VCS was Git.",
